212a0191a6bd83ffcfdeb9c0581243c50e0bbd38
David <53689532+RelativelyFine@users.noreply.github.com>
2021-11-07 02:41:08 -0500

    Fixed

--- backend/maps-api/places.js
+++ backend/maps-api/places.js
@@ -1,24 +1,23 @@
 var RADIUS = 5000
-var LAT = 51.5287352
-var LNG = -0.3817841
 var ZIP = "L4B3G4"
 const API_KEY = "AIzaSyAp5Wy8XyfO2a6U0eKlFns8NhDbxFieRx0"
 
-async function getCoordinates(ZIP){
-    fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+ZIP+'&key='+API_KEY + '&region=ca')
+
+function getCoordinates(ZIP, _callback){
+    fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+ ZIP +'&key='+ API_KEY + '&region=ca')
       .then(response => response.json())
       .then(data => {
         LAT = data.results[0].geometry.location.lat;
         LNG = data.results[0].geometry.location.lng;
+        console.log(LAT,LNG)
+        _callback();
       })
+      
   };
 
-async function findPlaces(){
-
-    await getCoordinates(ZIP)
-
-
-    const request = {
+function findPlaces(){
+    
+    let request = {
         location: new google.maps.LatLng(LAT, LNG),
         keyword: 'mental health',
         radius: RADIUS
@@ -51,4 +50,4 @@ async function findPlaces(){
 
 };
 
-await findPlaces()
+getCoordinates(ZIP, findPlaces);
